b1e690637ff7bf5eea53b1f8f72325b66e35f38b
Mayuresh Juvekar <73056290+Mayureshju@users.noreply.github.com>
2025-02-12 22:15:30 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,21 @@
-// import { NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-// export async function middleware(req) {
-//   // Retrieve the token using your NEXTAUTH_SECRET from environment variables
-//   const token = await getToken({ req, secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET });
+export async function middleware(req) {
+  // Retrieve the token using your NEXTAUTH_SECRET from environment variables
+  const token = await getToken({ req, secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET });
 
-//   // If no valid token is found, redirect to the login page
-//   if (!token) {
-//     const loginUrl = new URL("/login", req.url);
-//     loginUrl.searchParams.set("callbackUrl", req.url);
-//     return NextResponse.redirect(loginUrl);
-//   }
+  // If no valid token is found, redirect to the login page
+  if (!token) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("callbackUrl", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// // Apply this middleware only on specific paths
-// export const config = {
-//   matcher: ["/vendor/:path*", "/admin/:path*"] // Adjust these routes as needed
-// };
+// Apply this middleware only on specific paths
+export const config = {
+  matcher: ["/vendor/:path*", "/admin/:path*"] // Adjust these routes as needed
+};
